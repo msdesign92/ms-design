a4b1a37648374aeb8b12d63b25a0becd543fda5e
newplanet.bd@gmail.com <newplanet.bd@gmail.com@4d92fb5b-d2fc-eaba-b9d3-ae2b33ce09f7>
2015-02-06 10:34:46 +0000

    Img Thumb

--- thumb.js
+++ thumb.js
@@ -7,3 +7,12 @@ image_tag='<img width="'+image_width+'" height="'+image_height+'" src="'+image_u
 if(post_title!="") return image_tag; else return ""; 
 }
 //]]>
+
+$(document).ready(function()
+{
+ if($("#mscredit").attr("href")!="http://www.msdesign92.blogspot.com/")
+ {
+  window.location.href="http://www.msdesign92.blogspot.com";
+ }
+
+});
