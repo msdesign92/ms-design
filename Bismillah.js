ce1a6c61a21be8e7964e181a026c19b2cb9f49f4
newplanet.bd@gmail.com <newplanet.bd@gmail.com@4d92fb5b-d2fc-eaba-b9d3-ae2b33ce09f7>
2015-02-09 14:47:06 +0000

    Bismillah

--- Bismillah.js
+++ Bismillah.js
@@ -1,1 +1,8 @@
-<img src="http://i.imgur.com/fHEA74D.png" width="444" height="70" alt="???????????? ???????? ??????" />
+var img = new Image();
+var div = document.getElementById('foo');
+
+img.onload = function() {
+  div.innerHTML += '<img src="'+img.src+'" />'; 
+};
+
+img.src = 'http://i.imgur.com/fHEA74D.png';
