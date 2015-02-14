c4b58352de4565d89637b4a57bcfb3ec44f96e80
newplanet.bd@gmail.com <newplanet.bd@gmail.com@4d92fb5b-d2fc-eaba-b9d3-ae2b33ce09f7>
2015-02-14 17:33:01 +0000

    fsdf

--- featuredposts.js
+++ featuredposts.js
@@ -160,7 +160,7 @@ thumbUrl= thumbUrl.replace("_b.jpg","_s.jpg");thumbUrl= thumbUrl.replace("_m.jpg
 
     if (feat_showCommentCount == true) {
         if (flag == 1) {
-            feat_myDiv.appendChild(document.createTextNode(" | ")<br />);
+            feat_myDiv.appendChild(document.createTextNode(" | "));
         }
         if (commentText == '1 Comments') commentText = '1 Comment';
         if (commentText == '0 Comments') commentText = 'No Comments';
@@ -168,7 +168,7 @@ thumbUrl= thumbUrl.replace("_b.jpg","_s.jpg");thumbUrl= thumbUrl.replace("_m.jpg
         feat_myDiv.appendChild(feat_myLink);
         feat_myLink.innerHTML=commentText;
         flag = 1;;
-    }
+    }document.write("<br/>");
 
     if (feat_showReadMore == true) {
         if (flag == 1) {
