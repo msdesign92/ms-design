e0513bcd08824bec5cb4fc9e30b85bcf5014054e
newplanet.bd@gmail.com <newplanet.bd@gmail.com@4d92fb5b-d2fc-eaba-b9d3-ae2b33ce09f7>
2015-02-17 09:04:07 +0000

    Readmore

--- auto-readmore-blogger.js
+++ auto-readmore-blogger.js
@@ -15,15 +15,6 @@ function removeHtmlTag(strx,chop){
 	return strx+'...'; 
 }
 
-function createSummaryAndThumb(pID){
-	var div = document.getElementById(pID);
-	var imgtag = "";
-	var img = div.getElementsByTagName("img");
-	var summ = summary_noimg;
-	if(img.length>=1) {	
-		imgtag = '<span style="float:left; padding:0px 10px 5px 0px;"><img src="'+img[0].src+'" width="'+img_thumb_width+'px" height="'+img_thumb_height+'px"/></span>';
-		summ = summary_img;
-	}
 	
 	var summary = imgtag + '<div>' + removeHtmlTag(div.innerHTML,summ) + '</div>';
 	div.innerHTML = summary;
