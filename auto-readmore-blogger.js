5a289f365fd710e4f535b17f3d5d4b58c55e8b83
newplanet.bd@gmail.com <newplanet.bd@gmail.com@4d92fb5b-d2fc-eaba-b9d3-ae2b33ce09f7>
2015-02-17 09:25:46 +0000

    Readmoreee

--- auto-readmore-blogger.js
+++ auto-readmore-blogger.js
@@ -16,6 +16,14 @@ function removeHtmlTag(strx,chop){
 }
 
 function createSummaryAndThumb(pID){
+	var div = document.getElementById(pID);
+	var imgtag = "";
+	var img = div.getElementsByTagName("img");
+	var summ = summary_noimg;
+	if(img.length>=1) {	
+  
+	}
+	
 	var summary = imgtag + '<div>' + removeHtmlTag(div.innerHTML,summ) + '</div>';
 	div.innerHTML = summary;
 }
