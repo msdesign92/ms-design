4c46af05ac4e3803d597fe8379ecae6bc112fb80
Muhammad Subel <newplanet.bd@gmail.com>
2015-04-30 21:27:17 +0600

    Update and rename recent-comments3.js to recent.js

--- recent.js
+++ recent.js
@@ -78,6 +78,6 @@ var numComments = numComments || 5, avatarSize = avatarSize || 60, characters = 
         if (hideCredits == true) {
             hideCSS = "display:none;";
         }
-        commentsHtml += "<span style=\"font-size:8px;display:block;text-align:right;" + hideCSS + "\">dimodifikasi oleh <a href=\"http://afandi.ok-rek.com\" target=\"_blank\">Afandi Kusuma</a><br>dari: way2blogging</span>";
+        commentsHtml += "<span style=\"font-size:8px;display:block;text-align:right;" + hideCSS + "\">widget by <a href=\"http://msdesign92.blogspot.com\" target=\"_blank\">MS Design</a></span>";
         document.write(commentsHtml);
     }
