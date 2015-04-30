3dcebb9f5182a115558a7fa4b314fb0534e45a0b
Muhammad Subel <newplanet.bd@gmail.com>
2015-04-30 21:16:53 +0600

    Update recent-comment.js

--- recent-comment.js
+++ recent-comment.js
@@ -78,6 +78,6 @@ var numComments = numComments || 5, avatarSize = avatarSize || 60, characters = 
         if (hideCredits == true) {
             hideCSS = "display:none;";
         }
-        commentsHtml += "<span style=\"font-size:8px;display:block;text-align:right;" + hideCSS + "\">MS Design <a href=\"http://afandi.ok-rek.com\" target=\"_blank\">MS Design</a></span>";
+        commentsHtml += "<span style=\"font-size:8px;display:block;text-align:right;" + hideCSS + "\">Widget by: <a href=\"http://msdesign92.blogspot.com" target=\"_blank\">MS Design</a></span>";
         document.write(commentsHtml);
     }
