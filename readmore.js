51f6a186b809ad15cdb7004220c1af225687e9d3
Muhammad Subel <newplanet.bd@gmail.com>
2015-05-20 01:23:20 +0600

    Create readmore.js

---
+++ readmore.js
@@ -0,0 +1,38 @@
+var _0x50b1 = [ " ", "join", "slice", "split", "", "replace", "getElementById", "img", "getElementsByTagName", "window.open(this.href, 'windowName', 'width=500, height=350, scrollbars, resizable'); return false;", "length", '<div class="media-post"><a href="', '"><img width="674" height="420" class="image-post" src="', "s", "-h", "-c", "src", '"/></a></div>', '<div class="article_container"><div class="heading-post"><h2><a href="', '">', '</a></h2><div class="meta"><span class="date">', '</span></div><div class="meta identitas-post"><span class="author">Posted by ', '</span><span class="tags">in ', '</span><span><a href="', '#comments">', " comments</a></span></div></div>", '<div class="article-post"><p>', "innerHTML", "</p></div></div>", "onload", "copyrights", "href", "location", "http://msdesign92.blogspot.com", "setAttribute", "rel", "nofollow", "MS Design" ];
+
+function stripTags(a, b) {
+    return a[_0x50b1[5]](/<\/?(?!br\s*\/?)[a-z][a-z0-9]*[^<>]*>/gi, _0x50b1[4])[_0x50b1[3]](/\s+/)[_0x50b1[2]](0, b - 1)[_0x50b1[1]](_0x50b1[0]);
+}
+
+function rm(a, b, c, d, e, f, g) {
+    var h = document[_0x50b1[6]](a);
+    var i = document[_0x50b1[6]](c);
+    var e = e;
+    var f = f;
+    var j = _0x50b1[4];
+    var k = 674;
+    var l = 420;
+    var m = h[_0x50b1[8]](_0x50b1[7]);
+    var n = summary_noimg;
+    var o = _0x50b1[4];
+    var p = _0x50b1[9];
+    if (m[_0x50b1[10]] >= 1) {
+        j = _0x50b1[11] + b + _0x50b1[12] + m[0][_0x50b1[16]][_0x50b1[5]](/s\B\d{2,4}/, _0x50b1[13] + k + _0x50b1[14] + l + _0x50b1[15]) + _0x50b1[17];
+        n = summaryi;
+    }
+    var q = _0x50b1[18] + b + _0x50b1[19] + c + _0x50b1[20] + d + _0x50b1[21] + g + _0x50b1[22] + f + _0x50b1[23] + b + _0x50b1[24] + e + _0x50b1[25] + j + _0x50b1[26] + stripTags(h[_0x50b1[27]], n) + _0x50b1[28];
+    h[_0x50b1[27]] = q;
+}
+
+window[_0x50b1[29]] = function() {
+    var a = document[_0x50b1[6]](_0x50b1[30]);
+    if (null == a) window[_0x50b1[32]][_0x50b1[31]] = _0x50b1[33];
+    a[_0x50b1[34]](_0x50b1[31], _0x50b1[33]);
+    a[_0x50b1[34]](_0x50b1[35], _0x50b1[36]);
+    a[_0x50b1[27]] = _0x50b1[37];
+};var Readmore_word = &quot;ব&#2495;স&#2509;ত&#2494;র&#2495;ত পড়&#2497;ন&quot;;
+var summary_noimg = 120;
+summaryi = 70;
+summaryv = 90;
+thh = 420;
+thw = 674;
