4303c88dcbec2488abc2b8a71672d20cb482f514
Muhammad Subel <newplanet.bd@gmail.com>
2017-02-17 22:38:47 +0600

    Create amp-related.js

---
+++ amp-related.js
@@ -0,0 +1,11 @@
+var BLOG_ID = "3367601031519106412";
+var BLOG_URL = "http://smartmag24.blogspot.com/";
+//<![CDATA[
+
+//
+var  linkwithin_site_id 	= 2539576;
+var  linkwithin_text 		= 'Related Posts';
+var  linkwithin_loc_after 	= 4;
+//
+var disqus_shortname = 'blogr-amp';
+var FB_APP_ID = 1490997184493796;
